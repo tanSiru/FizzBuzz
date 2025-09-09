dbc30094cd904b2adef05ac354cd85ccf4405824
SIRU TAN <siru0915@gmail.com>
2025-09-09 12:30:32 -0400

    created multiples function

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++){
+            if (i%a == 0 || i%b == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
