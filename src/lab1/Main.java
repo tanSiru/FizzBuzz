7abe22efcd2b8061b73e87498b7cabbc64c71f46
SIRU TAN <siru0915@gmail.com>
2025-09-09 11:57:17 -0400

    seeing private method in action

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+//        FizzBuzz.doFizzBuzz(5);
+        String[] tst = {"1"};
+        FizzBuzz.main(tst);
+    }
+}
